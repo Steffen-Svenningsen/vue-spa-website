738b3e351cd0fce5d651ad61805dc172322ef0e8
Steffen-Svenningsen <steffen@zynaps.dk>
2022-11-12 19:12:51 +0100

    Add a reusable button component

---
+++ src/components/ButtonComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <button>
+    <slot></slot>
+  </button>
+</template>
+
+<style lang="sass" scoped>
+@import ../assets/global
+
+button
+    color: $background
+    border: none
+    cursor: pointer
+    padding: 1rem
+    transition: 0.2s
+    background: $black
+    font-family: "Montserrat", sans-serif
+
+    &:hover
+        transform: scale(0.95)
+</style>
