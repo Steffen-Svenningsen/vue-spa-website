7b97a4c7da69d73ef3ac14480e1b6dc23dcbf798
Steffen-Svenningsen <steffen@zynaps.dk>
2022-11-15 08:44:37 +0100

    Refactor contact view and add contact form component to the page

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,11 +1,48 @@
+<script setup lang="ts">
+import ContactForm from "@/components/ContactForm.vue";
+</script>
+
 <template>
   <div class="contact">
-    <h1>This is a contact page</h1>
-    <p>helo</p>
+    <div class="contact-form">
+      <h1>Send us a message here</h1>
+      <ContactForm />
+    </div>
+    <div>
+      <img
+        src="@/assets/Service.svg"
+        width="500"
+        height="500"
+        alt="Customer service"
+      />
+    </div>
   </div>
 </template>
 
 <style lang="sass">
+@import ../assets/global.sass
+
 .contact
-  padding: 1rem 2.5rem
+  padding: 4rem
+  display: flex
+  flex-wrap: wrap
+  justify-content: space-between
+
+  div
+
+    img
+      width: 100%
+
+  .contact-form
+    max-width: 600px
+    min-width: 500px
+
+    h1
+      font-size: 2rem
+      font-family: "Moglan", sans-serif
+      padding-left: 1rem
+
+@media (max-width:650px)
+  .contact-form
+    min-width: 100% !important
 </style>
